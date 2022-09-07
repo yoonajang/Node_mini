d0a62ba4649772c1b14264ffa28ba377c2228e16
장윤아 <99785621+yoonajang@users.noreply.github.com>
2022-09-07 14:12:31 +0900

    Test3 (#4)
    * a
    
    * b
    
    * c

--- routes/user.js
+++ routes/user.js
@@ -2,21 +2,21 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/auth-middleware");
 
-const { signup, login, isLogin } = require("../controllers/userController");
-
-// c
-
-// 회원가입
+// 회원가입2
 router.post("/signup", signup);
 
-//b
+// 테스트 1
+router.post("/test", test);
 
-// 로그인
-router.post("/login", login);
-
-// a
+// 테스트 2
+router.post("/test", test2);
 
 // 로그인 유효성 검사
 router.get("/islogin", authMiddleware, isLogin);
 
+// 로그인
+router.post("/login", login);
+
 module.exports = router;
+
+const { signup, login, isLogin } = require("../controllers/userController");
