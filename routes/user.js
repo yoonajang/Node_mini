dee75ed3664d5e319841bc513be476d2abcdf42e
yoonaj <o119a@naver.com>
2022-09-24 15:24:52 +0900

    Fixes: 회원 관리 관련 수정 완료

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/auth-middleware");
+const { signup, login, isLogin } = require("../controllers/userController");
 
-// 회원가입2
+// 회원가입
 router.post("/signup", signup);
-
-// 테스트 1
-router.post("/test", test);
-
-// 테스트 2
-router.post("/test", test2);
 
 // 로그인 유효성 검사
 router.get("/islogin", authMiddleware, isLogin);
@@ -18,5 +13,3 @@ router.get("/islogin", authMiddleware, isLogin);
 router.post("/login", login);
 
 module.exports = router;
-
-const { signup, login, isLogin } = require("../controllers/userController");
